60041f82fe873c9cc223efd7331203c30e0fd4ec
Bayram Eren <bayram.eren@outlook.com.tr>
2023-05-08 00:19:36 +0300

    hom controller added

---
+++ TestProject/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
