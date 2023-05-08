43e53dd441e196c5a26489b4e853e6fb710baf90
Bayram Eren <bayram.eren@outlook.com.tr>
2023-05-08 12:10:53 +0300

    test

--- TestProject/Controllers/HomeController.cs
+++ TestProject/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace TestProject.Controllers
             return Ok();
         }
 
-        [HttpGet("testas")]
+        [HttpGet("testasa")]
         public IActionResult Test()
         {
             return Ok();
