8e8d11c583cb61b928a07ba96aa75674d92b51c8
Bayram Eren <bayram.eren@outlook.com.tr>
2023-05-08 12:03:02 +0300

    test

---
+++ TestProject/Controllers/HomeController - Copy.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok();
+        }
+
+        [HttpGet("testa")]
+        public IActionResult Testa()
+        {
+            return Ok();
+        }
+
+        [HttpGet("testaa")]
+        public IActionResult Testaa()
+        {
+            return Ok();
+        }
+
+        [HttpGet("testaaasd")]
+        public IActionResult Testaaasd()
+        {
+            return Ok();
+        }
+
+        [HttpGet("MesutCancik2")]
+        public IActionResult MesutCancik()
+        {
+            return Ok();
+        }
+    }
+}
